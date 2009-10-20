4bc9d78f9c4262c11580cd82044c62e467a02f2e
rajavelu <rajavelu@c7f7b940-bd80-11de-82ee-51df2961d3cf>
2009-10-20 15:35:54 +0000

    Empty placeholder class

---
+++ edicoder/src/com/googlecode/edicoder/EDIEncoder.java
@@ -0,0 +1,40 @@
+package com.googlecode.edicoder;
+
+import javax.xml.transform.Source;
+
+public class EDIEncoder {
+	
+	public Source decodeFromString(String input) {
+		// FIXME implement decode
+		return null;
+	}
+	
+	public String encodeToString(Source xmlSource) {
+		// FIXME implement encode
+		return null;
+	}
+	
+	public Source decodeFromBytes(byte[] input) {
+		// FIXME implement decode
+		return null;
+	}
+	
+	public byte[] encodeToBytes(Source xmlSource) {
+		// FIXME implement encode
+		return null;
+	}
+	
+	public boolean dispose() {
+		// TODO dispose resources
+		return true;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
